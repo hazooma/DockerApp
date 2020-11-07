4bdad2be1ed8aaa59b74787d9cb33369fe7d6def
Hazem <abdelalimhazem@gmail.com>
2020-11-07 13:11:18 +0100

    Fix Docker :bug:

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,33 @@
 FROM node:12.10-alpine
 
-USER nobody
 
-# specify the working directory
-WORKDIR app
+# create root application folder
+WORKDIR /app
+
+
+COPY package*.json ./
+RUN npm install
+
+# copy source code to /app/src folder
+ADD . .
+
+RUN npm run build
+
+
+COPY tsconfig.json ./dist/
+COPY .env ./dist/
+
+WORKDIR ./dist/
+
+
+
+# check files list
+RUN ls -a
 
 # expose server and debug port
 EXPOSE 8080 5858
 
+
+
 # run application
-CMD ["node", "dist/src/index.js"]
+CMD ["node", "src/index.js"]
--- src/server/movie/controller.ts
+++ src/server/movie/controller.ts
@@ -7,32 +7,32 @@ export class MovieController {
          private manager: MovieManager;
 
          constructor(manager: MovieManager) {
-           this.manager = manager;
+           this.manager = manager;;
          }
 
          public async create(ctx: Context) {
            const movieBody: CreateMovie = ctx.request.body;
            const newMovie = await this.manager.create(movieBody as Movie);
 
-           ctx.body = new MovieModel(newMovie);
-           ctx.status = 201;
+           ctx.body = new MovieModel(newMovie);;
+           ctx.status = 201;;
          }
 
          public async getAll(ctx: Context) {
-           const movies = await this.manager.getAll();
-           ctx.body = movies;
-           ctx.status = 200;
+           const movies = await this.manager.getAll();;
+           ctx.body = movies;;
+           ctx.status = 200;;
          }
 
          public async get(ctx: Context) {
-           const movie = await this.manager.find(ctx.params.id);
-           ctx.body = new MovieModel(movie);
-           ctx.status = 200;
+           const movie = await this.manager.find(ctx.params.id);;
+           ctx.body = new MovieModel(movie);;
+           ctx.status = 200;;
          }
 
          public async getMovieReviews(ctx: Context) {
-           const reviews = await this.manager.findMovieReviews(ctx.params.id);
-           ctx.body = reviews;
-           ctx.status = 200;
+           const reviews = await this.manager.findMovieReviews(ctx.params.id);;
+           ctx.body = reviews;;
+           ctx.status = 200;;
          }
        }
